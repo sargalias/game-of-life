30df59e40806d94a98e87add06014e49c8ce8690
Spyros Argalias <sargalias@gmail.com>
2018-07-14 08:08:27 +0100

    clean up React code

--- src/App.js
+++ src/App.js
@@ -2,45 +2,18 @@ import React, { Component } from 'react';
 import Container from './components/Container';
 import Title from './components/Title';
 import Generation from './components/Generation';
-import Canvas from './components/Canvas';
 import Buttons from './components/Buttons';
 import Attribution from './components/Attribution';
 
 import './App.css';
 
-import Board from './logic/Board';
-import Rules from './logic/GameOfLifeRules';
-import Drawer from './logic/Drawer';
-import Square from './logic/Square';
-
-const squares = [];
-for (let x=0; x<500; x++) {
-  for (let y=0; y<500; y++) {
-    if (Math.random() < 0.05) {
-      squares.push(new Square(x, y));
-    }
-  }
-}
-let board = new Board(squares);
-const rules = new Rules();
-const drawer = new Drawer();
-drawer.draw(board);
-// setInterval(() => {
-//   board = rules.run(board);
-//   drawer.draw(board);
-// }, 500);
 
 class App extends Component {
-  state = {
-    generation: 0
-  };
-
   render() {
     return (
       <Container>
         <Title title="Game Of Life" />
         <Generation />
-        <Canvas />
         <Buttons />
         <Attribution
           authorName="Spyros Argalias"
--- src/components/Canvas.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Canvas = () => (
-  <div id="canvas-container">
-    <canvas id="canvas" />
-  </div>
-);
-
-export default Canvas;
