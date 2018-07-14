daaecc6d576778d349a237e96a429424cce5e025
Spyros Argalias <sargalias@gmail.com>
2018-07-14 20:22:01 +0100

    remove unused code

--- src/tests/components/Canvas.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Canvas from '../../components/Canvas';
-
-test('should render Canvas correctly', () => {
-  const wrapper = shallow(<Canvas />);
-  expect(wrapper).toMatchSnapshot();
-});
