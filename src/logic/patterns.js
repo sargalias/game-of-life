c230a87826dbb6680b5a805bfa07ac4426ab8b71
Spyros Argalias <sargalias@gmail.com>
2018-07-17 09:27:07 +0100

    add applyPattern function

--- src/logic/patterns.js
+++ src/logic/patterns.js
@@ -1,12 +1,28 @@
 import { copyBoard } from "./board";
 
-export const applyPattern = (board, pattern, x, y) => {
+export const applyPattern = (board, pattern, xBoard, yBoard) => {
   // Returns a new board.
   // board: 2D array
   // pattern: 2D array
   const newBoard = copyBoard(board);
-  if (pattern.length === 0)
-    return newBoard;
+  for (let xPattern=0; xPattern<pattern.length; xPattern++) {
+    for (let yPattern=0; yPattern<pattern[xPattern].length; yPattern++) {
+      handleVal(newBoard, board, pattern, xBoard, yBoard, xPattern, yPattern);
+    }
+  }
+  return newBoard
+};
+
+const handleVal = (newBoard, board, pattern, xBoard, yBoard, xPattern, yPattern) => {
+  const patternVal = pattern[xPattern][yPattern];
+  if (patternVal !== 0) {
+    // index range check
+    if (newBoard.length > xBoard + xPattern && newBoard[xBoard+xPattern].length > yBoard+yPattern) {
+      const curVal = newBoard[xBoard+xPattern][yBoard+yPattern];
+      const newVal = curVal === 0 ? pattern[xPattern][yPattern] : 0;
+      newBoard[xBoard+xPattern][yBoard+yPattern] = newVal;
+    }
+  }
 };
 
 export const getPattern = (filepath) => {
--- src/tests/logic/patterns.test.js
+++ src/tests/logic/patterns.test.js
@@ -6,17 +6,18 @@ const patternPath = path.join(__dirname, '..', 'patterns');
 const simplePattern = path.join(patternPath, 'simple-pattern_106.lif.txt');
 const normalPattern = path.join(patternPath, '34p13_106.lif.txt');
 
+
 test('applyPattern returns new board', () => {
   const emptyBoard = boards['empty'];
   const pattern = [];
   const x = 0;
   const y = 0;
   const newBoard = applyPattern(emptyBoard, pattern, x, y);
-  emptyBoard[0][0] = 1;
+  newBoard[0][0] = 1;
   expect(emptyBoard).not.toEqual(newBoard);
 });
 
-test('applyPattern works correctly with empty board, empty pattern, x=0, y=0', () => {
+test('applyPattern should work correctly with empty board, empty pattern, x=0, y=0', () => {
   const emptyBoard = boards['empty'];
   const pattern = [];
   const x = 0;
@@ -24,7 +25,7 @@ test('applyPattern works correctly with empty board, empty pattern, x=0, y=0', (
   expect(applyPattern(emptyBoard, pattern, x, y)).toEqual(emptyBoard);
 });
 
-test('applyPattern works correctly with non-empty board and empty pattern, x=0, y=0', () => {
+test('applyPattern should work correctly with non-empty board and empty pattern, x=0, y=0', () => {
   const fullBoard = boards['full'];
   const pattern = [];
   const x = 0;
@@ -32,10 +33,133 @@ test('applyPattern works correctly with non-empty board and empty pattern, x=0, 
   expect(applyPattern(fullBoard, pattern, x, y)).toEqual(fullBoard);
 });
 
-test('applyPattern works correctly with non-empty board and empty pattern', () => {
+test('applyPattern should work correctly with non-empty board and empty pattern', () => {
   const fullBoard = boards['full'];
   const pattern = [];
   const x = 0;
   const y = 0;
   expect(applyPattern(fullBoard, pattern, x, y)).toEqual(fullBoard);
+});
+
+test('applyPattern should return a new board', () => {
+  const fullBoard = boards['full'];
+  const pattern = [];
+  const x = 0;
+  const y = 0;
+  const newBoard = applyPattern(fullBoard, pattern, x, y);
+  newBoard[0][0] = 5;
+  expect(fullBoard).not.toEqual(newBoard);
+});
+
+test('applyPattern should correctly apply pattern with x=0 y=0 1', () => {
+  const emptyBoard = [
+    [0, 0],
+    [0, 0]
+  ];
+  const pattern = [
+    [1, 0],
+    [0, 1]
+  ];
+  const x = 0;
+  const y = 0;
+  const expected = [
+    [1, 0],
+    [0, 1]
+  ];
+  expect(applyPattern(emptyBoard, pattern, x, y)).toEqual(expected);
+});
+
+test('applyPattern should correctly apply pattern with x=0 y=0 2', () => {
+  const emptyBoard = [
+    [0, 0],
+    [0, 0]
+  ];
+  const pattern = [
+    [1, 0],
+    [1, 0]
+  ];
+  const x = 0;
+  const y = 0;
+  const expected = [
+    [1, 0],
+    [1, 0]
+  ];
+  expect(applyPattern(emptyBoard, pattern, x, y)).toEqual(expected);
+});
+
+test('applyPattern should correctly apply pattern with x=0 y=0 3', () => {
+  const emptyBoard = boards['empty'];
+  const pattern = [
+    [1, 0],
+    [0, 1]
+  ];
+  const x = 0;
+  const y = 0;
+  const expected = [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 0],
+  ];
+  const newBoard = applyPattern(emptyBoard, pattern, x, y);
+  expect(newBoard).toEqual(expected);
+});
+
+test('applyPattern should correctly apply pattern on non-empty board', () => {
+  const emptyBoard = boards['full'];
+  const pattern = [
+    [1, 0],
+    [0, 1]
+  ];
+  const x = 0;
+  const y = 0;
+  const expected = [
+    [0, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1],
+  ];
+  const newBoard = applyPattern(emptyBoard, pattern, x, y);
+  expect(newBoard).toEqual(expected);
+});
+
+test('applyPattern should correctly apply pattern with non-0 x and y', () => {
+  const emptyBoard = boards['empty'];
+  const pattern = [
+    [1, 0],
+    [0, 1]
+  ];
+  const x = 1;
+  const y = 1;
+  const expected = [
+    [0, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1]
+  ];
+  expect(applyPattern(emptyBoard, pattern, x, y)).toEqual(expected);
+});
+
+test('applyPattern should ignore out of range elements 1', () => {
+  const emptyBoard = boards['empty'];
+  const pattern = [
+    [1, 0],
+    [0, 1]
+  ];
+  const x = 5;
+  const y = 5;
+  expect(applyPattern(emptyBoard, pattern, x, y)).toEqual(emptyBoard);
+});
+
+test('applyPattern should ignore out of range elements 2', () => {
+  const emptyBoard = boards['empty'];
+  const pattern = [
+    [1, 0],
+    [0, 1]
+  ];
+  const x = 2;
+  const y = 2;
+  const expected = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 1]
+  ];
+  expect(applyPattern(emptyBoard, pattern, x, y)).toEqual(expected);
 });
