927ffeb2488ee40773e60fa01689cca382c3f27a
Spyros Argalias <sargalias@gmail.com>
2018-07-17 14:02:07 +0100

    work on patternParser

--- src/test/patternParser.js
+++ src/test/patternParser.js
@@ -3,11 +3,25 @@ const fs = require('fs');
 
 const patternParser = (inDir, outDir) => {
   // get all files
+  getAllFilenames(inDir)
+    .then(filename => {
+
+    });
   return null;
 };
 
-const parsePattern = (file) => {
+const parseFile = (filepath) => {
+  return readFile(filepath)
+    .then(parsePattern);
+};
 
+const readFile = (filepath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filepath, 'utf8', (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
 };
 
 const getAllFilenames = (dir) => {
@@ -19,5 +33,39 @@ const getAllFilenames = (dir) => {
   });
 };
 
+const parsePattern = (data) => {
+  const board = [];
+  let name = 'bob';
+  const lines = data.trim().split('\n');
+  for (let line of lines) {
+    line = line.trim();
+    if (line.startsWith('#')) {
+      if (line.includes('Name')) {
+        name = parseName(line);
+        let match = line.match(/Name:\s+(.*)/);
+        if (match) {
+          name = match[1];
+        }
+      }
+    }
+    else {
+      const lineArr = [];
+      for (let symbol of line) {
+        if (symbol === '.') {
+          lineArr.push(0);
+        } else {
+          lineArr.push(1);
+        }
+      }
+      board.push(lineArr);
+    }
+  }
+  return {[name]: board};
+};
 
-module.exports = { patternParser, parsePattern, getAllFilenames };
+const parseName = (line) => {
+
+};
+
+
+module.exports = { patternParser, parsePattern, getAllFilenames, readFile, parsePattern };
--- src/tests/test/patternParser.test.js
+++ src/tests/test/patternParser.test.js
@@ -1,20 +1,99 @@
 const path = require('path');
 const fs = require('fs');
-const { patternParser, parsePattern, getAllFilenames } = require('../../test/patternParser');
+const {
+  patternParser,
+  parsePattern,
+  getAllFilenames,
+  readFile
+} = require('../../test/patternParser');
 
 const inDir = path.join(__dirname, 'patterns_src');
 const outDir = path.join(__dirname, 'patterns_out');
 
 test('getAllFilenames works correctly', () => {
   expect.assertions(1);
-  const expected = ['34p13_105.lif', 'single_cell_105.lif'];
+  const expected = ['34p13_105.lif', 'simple_105.lif', 'single_cell_105.lif'];
   return getAllFilenames(inDir)
     .then(files => {
       expect(files).toEqual(expected);
     });
 });
 
-
-test('parsePattern works correctly', () => {
-
+test('readFile works correctly', () => {
+  const filepath = path.join(inDir, 'single_cell_105.lif');
+  return readFile(filepath).then((data) => {
+    expect(data).toMatchSnapshot();
+  });
 });
+
+test('parsePattern works correctly with single cell pattern', () => {
+  const data = `#Life 1.05
+#D Name: single_cell
+#D The smallest known period 13 oscillator.
+#D www.conwaylife.com/wiki/index.php?title=34P13
+#N
+#P -8 -8
+*`;
+  expect(parsePattern(data)).toEqual({'single_cell': [[1]]});
+});
+
+test('parsePattern works correctly with simple cell pattern', () => {
+  const data = `#Life 1.05
+#D Name: simple
+#D The smallest known period 13 oscillator.
+#D www.conwaylife.com/wiki/index.php?title=34P13
+#N
+#P -8 -8
+*.
+.*
+`;
+  expect(parsePattern(data)).toEqual({'simple': [[1,0], [0, 1]]});
+});
+
+test('parsePattern works correctly with normal cell pattern', () => {
+  const data = `#Life 1.05
+#D Name: 34P13
+#D The smallest known period 13 oscillator.
+#D www.conwaylife.com/wiki/index.php?title=34P13
+#N
+#P -8 -8
+....***
+.
+......*
+......**
+.....*
+........*
+....*...*
+.........**
+**..***.....*
+**.....*....**.*
+.......*...*...*
+.......*.*.....*
+.
+.
+......**
+......**
+`;
+  const expected = {
+    '34P13': [
+      [0,0,0,0,1,1,1],
+      [0],
+      [0,0,0,0,0,0,1],
+      [0,0,0,0,0,0,1,1],
+      [0,0,0,0,0,1],
+      [0,0,0,0,0,0,0,0,1],
+      [0,0,0,0,1,0,0,0,1],
+      [0,0,0,0,0,0,0,0,0,1,1],
+      [1,1,0,0,1,1,1,0,0,0,0,0,1],
+      [1,1,0,0,0,0,0,1,0,0,0,0,1,1,0,1],
+      [0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,1],
+      [0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,1],
+      [0],
+      [0],
+      [0,0,0,0,0,0,1,1],
+      [0,0,0,0,0,0,1,1]
+    ]
+  };
+  expect(parsePattern(data)).toEqual(expected);
+});
+
