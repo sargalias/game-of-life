453c0d200768275e8203d2762ed47a8665c8d7dd
Spyros Argalias <sargalias@gmail.com>
2018-07-19 21:32:31 +0100

    convert Board component to class, delete test tube pattern

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import Row from './Row';
 
-const Board = (props) => (
-  <div className="board">
-    {props.boardData.map((rowData, index) => (
-      <Row
-        key={index}
-        rowId={index}
-        rowData={rowData}
-        onCellClick={props.onCellClick}
-      />
-    ))}
-  </div>
-);
+class Board extends React.Component {
+  render() {
+    return (
+      <div className="board">
+        {this.props.boardData.map((rowData, index) => (
+          <Row
+            key={index}
+            rowId={index}
+            rowData={rowData}
+            onCellClick={this.props.onCellClick}
+          />
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Board;
