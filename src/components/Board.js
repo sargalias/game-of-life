4fd976877e3f63800a61838dae3c84c16d177323
Spyros Argalias <sargalias@gmail.com>
2018-07-19 21:45:59 +0100

    add colors array to Board if props.color is true

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import Row from './Row';
+import generateGradient from "../logic/color";
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    if (props.color)
+      this.state.colors  = generateGradient();
+  }
+
   render() {
     return (
       <div className="board">
--- src/tests/components/Board.test.js
+++ src/tests/components/Board.test.js
@@ -17,3 +17,23 @@ test('board should render correctly with boardData', () => {
   const wrapper = shallow(<Board boardData={boardData} onCellClick={() => {}} />);
   expect(wrapper).toMatchSnapshot();
 });
+
+test('board should initialise with a color array in state if props.color=true', () => {
+  const boardData = [
+    [0, 0, 0],
+    [1, 0, 1],
+    [0, 1, 0]
+  ];
+  const wrapper = shallow(<Board color={true} boardData={boardData} />);
+  expect(wrapper.state('colors')).toBeTruthy();
+});
+
+test('board should not initialise with a color array in state if props.color=false', () => {
+  const boardData = [
+    [0, 0, 0],
+    [1, 0, 1],
+    [0, 1, 0]
+  ];
+  const wrapper = shallow(<Board boardData={boardData} />);
+  expect(wrapper.state('colors')).toBeFalsy();
+});
