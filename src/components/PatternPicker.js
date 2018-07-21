ba5b3330548e5b1c16c8d69ae10e337953d7a4b1
Spyros Argalias <sargalias@gmail.com>
2018-07-21 16:44:35 +0100

    add readme and writeup

--- src/components/PatternPicker.js
+++ src/components/PatternPicker.js
@@ -4,6 +4,7 @@ const PatternPicker = ({patterns=[], onChange, value}) => (
   <div className="pattern-picker-container">
     <div>
       <label htmlFor="pattern" className="pattern-picker-text">Pattern:</label>
+      <p>(Click a cell on the board to add / remove pattern.)</p>
     </div>
     <select name="pattern" id="pattern" onChange={onChange} value={value}>
       {patterns.map((pattern) => (
